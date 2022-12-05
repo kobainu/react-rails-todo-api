7da4f25bfc666e5ab4ddd53b49139255fbee758b
小林大志 <kobayashihiroshi@kobayashihiroshinoMacBook-Air.local>
2022-12-05 23:44:45 +0900

    implement task creation function

--- todo-web/src/App.js
+++ todo-web/src/App.js
@@ -4,10 +4,20 @@ import axios from "axios";
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
+  const [name, setName] = useState("");
 
   const fetch = async () => {
     const res = await axios.get("http://localhost:3010/tasks");
     setTasks(res.data);
+  };
+
+  const createTask = async () => {
+    await axios.post("http://localhost:3010/tasks", {
+      name: name,
+      is_done: false,
+    });
+    setName("");
+    fetch();
   };
 
   useEffect(() => {
@@ -26,6 +36,14 @@ const App = () => {
     <>
       <div className="container">
         <h1>タスク一覧</h1>
+        <input
+          placeholder="タスク名を入力"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button colorScheme="teal" onClick={createTask}>
+          タスクを作成
+        </button>
         {tasks.map((task, index) => (
           <Task
             key={index}
