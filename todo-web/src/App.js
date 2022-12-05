1e1178965c28e1ca6614d2e4203bab83837e143a
小林大志 <kobayashihiroshi@kobayashihiroshinoMacBook-Air.local>
2022-12-05 22:58:41 +0900

    implement react hooks

--- todo-web/src/App.js
+++ todo-web/src/App.js
@@ -1,13 +1,49 @@
+import { useEffect, useState } from "react";
 import { Task } from "./component/Task";
 
 const App = () => {
+  const initialTasks = [
+    {
+      name: "買い物",
+      isDone: true,
+    },
+    {
+      name: "ランニング",
+      isDone: false,
+    },
+    {
+      name: "プログラミング",
+      isDone: false,
+    },
+  ];
+
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(initialTasks);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const toggleIsDone = (index) => {
+    const tasksCopy = [...tasks];
+    const isDone = tasksCopy[index].isDone;
+    tasksCopy[index].isDone = !isDone;
+    setTasks(tasksCopy);
+  };
+
   return (
     <>
       <div className="container">
         <h1>タスク一覧</h1>
-        <Task name="買い物" />
-        <Task name="ランニング" />
-        <Task name="勉強" />
+        {tasks.map((task, index) => (
+          <Task
+            key={index}
+            index={index}
+            name={task.name}
+            isDone={task.isDone}
+            toggleIsDone={toggleIsDone}
+          />
+        ))}
       </div>
     </>
   );
--- todo-web/src/component/Task.jsx
+++ todo-web/src/component/Task.jsx
@@ -1,7 +1,14 @@
 export const Task = (props) => {
   return (
     <div className="taskArea">
-      <input type="checkbox" className="task" />
+      <input
+        type="checkbox"
+        className="task"
+        isChecked={props.isDone}
+        onChange={() => {
+          props.toggleIsDone(props.index);
+        }}
+      />
       <label>{props.name}</label>
     </div>
   );
